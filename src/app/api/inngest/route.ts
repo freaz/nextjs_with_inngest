b53ce4ae19394d31ede4877f093e1dd4ae3019af
Ondřej Musil <mail@ondrejmusil.cz>
2023-12-11 16:17:14 +0100

    increase function duration

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,6 +2,8 @@ import { inngest } from "@/lib/inngest/client"
 import { processJob } from "@/lib/inngest/functions"
 import { serve } from "inngest/next"
 
+export const maxDuration = 10 // we want 300 byt I am on hobby
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
